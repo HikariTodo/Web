ccec363c8902baff86bc1fe2b2739e9dcf7cb8ac
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-11-12 00:17:09 +0100

    fix: projects loading on first load

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -1,5 +1,8 @@
 import { createResource } from "solid-js";
 import { getProjects } from "../api/projects";
+import database from "../database";
 
-export const [projects, { refetch: refetchProjects }] =
-  createResource(getProjects);
+export const [projects, { refetch: refetchProjects }] = createResource(
+  () => !database.loading && !database.error,
+  getProjects
+);
